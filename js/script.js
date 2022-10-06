d963fee99aa39fb0321c75ce9fe0bbd851acc0c8
Aida Brugnone <99371449+AidaB9837@users.noreply.github.com>
2022-10-06 18:28:38 +0200

    Add Typing animation in Home Section by cdnjs library's script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ------------------------- TYPING ANIMATION START ------------------------- */
+const typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Full-stack Developer",
+    "Web Developer",
+    "Front-end Developer",
+    "Back-end Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+/* -------------------------- TYPING ANIMATION END -------------------------- */
